a3d400f934c0f16cdbd145e326c2a4eb5302ad11
Tomasz Tomkielski <tomasz@tomkielski.com>
2017-06-24 13:46:36 +0200

    Pile

--- src/game/Game.js
+++ src/game/Game.js
@@ -3,22 +3,28 @@ import Pile from './Pile';
 export default class Game {
     constructor(deck) {
         this.deck = deck;
-        this.piles = [];
+        this.piles = this.constructor.buildPiles(deck);
+    }
+
+    getPiles() {
+        return this.piles;
+    }
+
+    static buildPiles(deck) {
+        const piles = [];
 
         for (let pile = 0; pile < 7; pile++) {
             const cards = new Pile();
 
             for (let i = 0; i < pile + 1; i++) {
-                cards.push(this.deck.popCard());
+                cards.push(deck.popCard());
             }
 
             cards.revealLast();
 
-            this.piles.push(cards);
+            piles.push(cards);
         }
-    }
 
-    getPiles() {
-        return this.piles;
+        return piles;
     }
 }
